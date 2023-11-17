416b01410f7bcfca0e022db20538181c99efff04
yadnyesh <yadnyesh2018@gmail.com>
2023-11-17 15:01:53 +0530

    tab spaces updated

--- slip_1_q1.h
+++ slip_1_q1.h
@@ -2,41 +2,41 @@
 #include<stdlib.h>
 struct node
 {
-int data;
-struct node *next,*prev;
+    int data;
+    struct node *next,*prev;
 };
 struct node *head=NULL,*nw,*ptr,*temp;
 void create()
 {
-int i,n;
-printf("Enter number of nodes : ");
-scanf("%d",&n);
-for(i=0;i<n;i++)
-{
-nw=(struct node*)malloc(sizeof(struct node));
-printf("\nEnter node data : ");
-scanf("%d",&nw->data);
-nw->next=NULL;
-nw->prev=NULL;
-if(head==NULL)
-{
-head=ptr=nw;
-}
-else
-{
-ptr->next=nw;
-nw->prev=ptr;
-ptr=ptr->next;
-}
-}
+    int i,n;
+    printf("Enter number of nodes : ");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        nw=(struct node*)malloc(sizeof(struct node));
+        printf("\nEnter node data : ");
+        scanf("%d",&nw->data);
+        nw->next=NULL;
+        nw->prev=NULL;
+        if(head==NULL)
+        {
+            head=ptr=nw;
+        }
+        else
+        {
+            ptr->next=nw;
+            nw->prev=ptr;
+            ptr=ptr->next;
+        }
+    }
 }
 void forward_display()
-{
-ptr=head;
-printf("\nLink list elements are :\n");
-while(ptr!=NULL)
-{
-printf("\t%d\t",ptr->data);
-ptr=ptr->next;
-}
+{   
+    ptr=head;
+    printf("\nLink list elements are :\n");
+    while(ptr!=NULL)
+    {
+        printf("\t%d\t",ptr->data);
+        ptr=ptr->next;
+    }
 }
